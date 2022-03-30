94e7eb0c2f1415b2d9a6a01dc34db820114fd6fb
maneywu <mwu019@fiu.edu>
2022-03-29 22:51:40 -0400

    initial commit
    + polaris.py
    • streamlit, pandas, and numpy imported
    • Basic outline

---
+++ polaris.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.set_page_config(
+    page_title="HCI - Polaris",
+    menu_items={
+        'About': '# Welcome to Polaris. Developed by David H. Rice, '\
+                 'Zedef Saias, Abbas Jaffer, Paul Pasco, Kenneth Currier,'\
+                 ' and Maney Wu for CAP 4104 HCI'
+    }
+)
+
+st.title("Welcome to Polaris")
+
+st.subheader("1 - Fill out this form to get started")
+
+st.subheader("2 - Map")
+
+st.subheader("3 - Want to save/print out the locations?")
