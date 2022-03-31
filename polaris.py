44e8810819d71459018952376149f912bc631ff5
ShadyBlaze <87132541+ShadyBlaze@users.noreply.github.com>
2022-03-31 18:47:59 -0400

    Update polaris.py

--- polaris.py
+++ polaris.py
@@ -1,20 +1,59 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+
+
 
 st.set_page_config(
-    page_title="HCI - Polaris",
-    menu_items={
-        'About': '# Welcome to Polaris. Developed by David H. Rice, '\
-                 'Zedef Saias, Abbas Jaffer, Paul Pasco, Kenneth Currier,'\
-                 ' and Maney Wu for CAP 4104 HCI'
-    }
+    page_title="Polaris"
+
 )
+st.title = ("Polaris")
+st.header("Polaris")
+st.subheader("The Hobby App")
 
-st.title("Welcome to Polaris")
+agree = st.checkbox ('Do you agree to submit your personal information to this application?')
+if(agree):
+ st.text("Enter your name")
+first_name=st.text_input('First Name')
+last_name=st.text_input('Last Name')
 
-st.subheader("1 - Fill out this form to get started")
+hobby = st.radio(
+     "Do you consider your hobbies extroverted or introverted?",
+     ('Extroverted', 'Introverted'))
 
-st.subheader("2 - Map")
+if hobby == 'Extroverted':
+ st.text('How extroverted do you think you are on a scale of 1-10?')
+ values = st.slider(
+     'Select a range of values',
+       0.0, 10.0, 5.0)
 
-st.subheader("3 - Want to save/print out the locations?")
+if hobby =="Introverted":
+ st.text("How introverted do you think you are on a scale of 1-10?")
+ values = st.slider(
+     'Select a range of values',
+       0.0, 10.0, 5.0)
+st.write('You are:', values, hobby)
+
+
+st.text("Where do you live?")
+lat = st.text_input("Latitude")
+lon = st.text_input("Longitude")
+
+if lat and lon:
+    df = pd.DataFrame({
+    'lat' : [float(lat)],
+    'lon':[float(lon)],})
+    st.dataframe(df)
+    st.map(df)
+    st.success("Map Successfully formed")
+
+
+else:st.error("Warning, empty or invalid coordinates")
+
+
+
+options = st.multiselect(
+     'What do you like?',
+     (['Sports', 'Gaming', 'Art', 'Music', 'Reading', 'Partying']))
+if options:
+ st.write('You like:', options)
