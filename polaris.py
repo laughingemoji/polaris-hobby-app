fe15ced53f84f8a786b5d73b9af8a8bf659d2a2e
ShadyBlaze <87132541+ShadyBlaze@users.noreply.github.com>
2022-04-09 22:53:53 -0400

    Update polaris.py
    Added button and bar graph. Need Data Table for last.

--- polaris.py
+++ polaris.py
@@ -4,7 +4,6 @@ import numpy as np
 
 st.set_page_config(
     page_title="Polaris",
-    page_icon= "📍",
     layout="wide",
     menu_items={
         'Get Help': 'https://docs.streamlit.io/',
@@ -17,50 +16,76 @@ st.set_page_config(
 
 st.title = "Polaris"
 st.header("Polaris: The Hobby App")
-st.write("👈Please fill out the interests form on the top left menu to get started.")
+st.write("Please fill out the interests form on the top left menu to get started.")
 
 coord1 = None
 coord2 = None
 
 # Hobbies/Interests Questionnaire
 with st.sidebar:
-    agree = st.checkbox('Click here to start!')
+    agree = st.checkbox('Do you agree to submit your personal information to this application?')
     if agree:
+        st.write("Enter your name")
+        first_name = st.text_input('First Name')
+        last_name = st.text_input('Last Name')
+        age = st.number_input('Your Age',min_value=1.0,
+         max_value=100.0,
+         step=1.0,
+        )
         # Location section
-        st.write("Enter a location: ")
+        st.write("Where do you live?")
         col1, col2 = st.columns(2)
         with col1:
             coord1 = st.text_input("Latitude")
         with col2:
             coord2 = st.text_input("Longitude")
 
-
-
         hobby = st.radio(
             "Do you consider your hobbies extroverted or introverted?",
             ('Extroverted', 'Introverted'))
         if hobby == 'Extroverted':
-            values = st.slider(
+            exvalues = st.slider(
                 'How extroverted are you on a scale of 1-10?',
                 0.0, 10.0, 5.0)
+            invalues=0
+            st.write('You are:', exvalues, hobby)
+
         if hobby == "Introverted":
-            values = st.slider(
+            invalues = st.slider(
                 'How introverted are you on a scale of 1-10?',
                 0.0, 10.0, 5.0)
-        st.write('You are:', values, hobby)
+            exvalues=0
+            st.write('You are:', invalues, hobby)
+
+        if exvalues==0:
+            exvalues= invalues-10
+            chart_data = pd.DataFrame({
+                "Introverted": [invalues],
+                "Extroverted": [exvalues]}
+                )
+
+        if invalues==0:
+            invalues=exvalues-10
+            chart_data = pd.DataFrame({
+                "Introverted": [invalues],
+                "Extroverted": [exvalues]}
+            )
+
+        st.bar_chart(chart_data)
+
 
         options = st.multiselect(
-            'What do you like to do for fun?',
+            'What do you like?',
             (['Sports', 'Gaming', 'Art', 'Music', 'Reading', 'Partying']))
         if options:
             st.write('You like:')
             for x in options:
-                st.text(x)
+                    st.text(x)
 
 col3, col4 = st.columns(2)
 with col3:
     # Displaying the Map
-    st.subheader("🗺️Map")
+    st.subheader("Map")
     if coord1 and coord2:
         df = pd.DataFrame({
             'lat': [float(coord1)],
@@ -71,6 +96,9 @@ with col3:
     else:
         st.error("Empty or invalid coordinates!")
 
+    result= st.button("When you are finished click")
+    if result:
+        st.write("Thank you ::thumbsup:")
 with col4:
     # Displaying Locations/Events/Addresses
-    st.subheader("📍Locations")
+    st.subheader("Locat
